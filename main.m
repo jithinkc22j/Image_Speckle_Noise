728b3aa6a739a50da521d0d9a83e9cb46deda828
JITHIN K C <jithinkc22@gmail.com>
2020-07-29 01:26:12 -1000

    Update main.m

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
  % Adding Noise to the images- speckle
 %------------------------------------------------------------------------------------------------------------------------------------
 % Note: If you are using my code for your system or project, you should always cite my paper as a reference. K C.Jithin, SyamSankar, 
-% "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set",
-% Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
+web('https://docs.google.com/document/d/1AbCxFoUhdOCppM8novgCdOv0F9mqYe7HlBU7yX7Svx0/edit?usp=sharing')
+web('published_work.html')
 %--------------------------------------------------------------------------------------------------------------------------------------
 
 % Demo: 
